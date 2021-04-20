0027e91ca614360694b69ae426c00740d3668288
AI678 <63541083+AI678@users.noreply.github.com>
2021-04-20 15:16:46 +0800

    修改__init__.py

--- Bigscity-TrafficDL/trafficdl/model/traffic_speed_prediction/__init__.py
+++ Bigscity-TrafficDL/trafficdl/model/traffic_speed_prediction/__init__.py
@@ -1,15 +1,15 @@
 from trafficdl.model.traffic_speed_prediction.DCRNN import DCRNN
-from trafficdl.model.traffic_speed_prediction.STGCN import STGCN
 from trafficdl.model.traffic_speed_prediction.GraphWaveNet import GWNET
 from trafficdl.model.traffic_speed_prediction.TGCLSTM import TGCLSTM
 from trafficdl.model.traffic_speed_prediction.TGCN import TGCN
 from trafficdl.model.traffic_speed_prediction.TemplateTSP import TemplateTSP
-
+from trafficdl.model.traffic_speed_prediction.STMGAT import STMGAT
 __all__ = [
     "DCRNN",
     "STGCN",
     "GWNET",
     "TGCLSTM",
     "TGCN",
-    "TemplateTSP"
+    "TemplateTSP",
+    "STMGAT"
 ]
